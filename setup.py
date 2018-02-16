d3d95bf66970dce0f93dd82deb4a6efd4b3ec42d
Scott Hansen <firecat4153@gmail.com>
2018-02-16 15:56:07 -0800

    Tag 0.3.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(name="keepmenu",
-      version="0.2.1",
+      version="0.3.0",
       description="Dmenu interface for Keepass",
       long_description=open('README.rst', 'rb').read().decode('utf-8'),
       author="Scott Hansen",
       author_email="firecat4153@gmail.com",
       url="https://github.com/firecat53/keepmenu",
-      download_url="https://github.com/firecat53/keepmenu/tarball/0.2.1",
+      download_url="https://github.com/firecat53/keepmenu/tarball/0.3.0",
       scripts=['keepmenu'],
       data_files=[('share/doc/keepmenu', ['README.rst', 'LICENSE.txt',
                                           'config.ini.example'])],
