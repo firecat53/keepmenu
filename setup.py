78328a4320f2d93324ade8140c1c8f8261174417
Scott Hansen <firecat4153@gmail.com>
2021-10-17 11:11:18 -0700

    Tested with python 3.10

--- setup.py
+++ setup.py
@@ -30,7 +30,8 @@ setup(name="keepmenu",
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
           'Topic :: Utilities',
       ],
       keywords=("dmenu keepass keepassxc"),
-     )
+      )
